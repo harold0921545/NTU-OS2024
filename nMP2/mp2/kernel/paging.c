b0c30c3c4a99b92b34aa5c74d006bce7b4110d58
shaman <haroldlin.0921@gmail.com>
2024-03-28 21:31:17 +0800

    complete pagefault handler

---
+++ nMP2/mp2/kernel/paging.c
@@ -0,0 +1,36 @@
+#include "param.h"
+#include "types.h"
+#include "memlayout.h"
+#include "riscv.h"
+#include "spinlock.h"
+#include "defs.h"
+#include "proc.h"
+
+/* NTU OS 2024 */
+/* Allocate eight consecutive disk blocks. */
+/* Save the content of the physical page in the pte */
+/* to the disk blocks and save the block-id into the */
+/* pte. */
+char *swap_page_from_pte(pte_t *pte) {
+  char *pa = (char*) PTE2PA(*pte);
+  uint dp = balloc_page(ROOTDEV);
+
+  write_page_to_disk(ROOTDEV, pa, dp); // write this page to disk
+  *pte = (BLOCKNO2PTE(dp) | PTE_FLAGS(*pte) | PTE_S) & ~PTE_V;
+
+  return pa;
+}
+
+/* NTU OS 2024 */
+/* Page fault handler */
+int handle_pgfault() {
+  /* Find the address that caused the fault */
+  uint64 va = r_stval();
+  struct proc *p = myproc();
+  pagetable_t pagetable = p->pagetable;
+
+  char *pa = kalloc();
+  memset(pa, 0, PGSIZE);
+  if(mappages(pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)pa, PTE_U|PTE_R|PTE_W|PTE_X) != 0)
+    kfree(pa);
+}
